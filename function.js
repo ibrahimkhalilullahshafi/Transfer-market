bd2af6fa2378da6f573286e162b2f3bf2e342658
ibrahimkhalilullahshafi <shafi.warx@gmail.com>
2022-08-23 16:38:31 +0600

    total player expense calculation done

--- function.js
+++ function.js
@@ -28,10 +28,15 @@ function getInputFieldValueById(inputFieldId) {
     const inputField = document.getElementById(inputFieldId);
     const inputFieldValueString = inputField.value;
     const inputFieldValue = parseFloat(inputFieldValueString);
+    inputField.value = '';
     return inputFieldValue
-
 }
 
-document.getElementById('calculate').addEventListener(click, function () {
-    const expense = getInputFieldValueById()
+
+document.getElementById('calculate').addEventListener('click', function () {
+    const expense = getInputFieldValueById('inputPerPlayer');
+    const playerNumber = document.getElementById('selected-list').children.length;
+    const playerExpenses = expense * playerNumber;
+    const playerExpensesTotal = document.getElementById('playerExpense');
+    playerExpensesTotal.innerText = playerExpenses;
 })
